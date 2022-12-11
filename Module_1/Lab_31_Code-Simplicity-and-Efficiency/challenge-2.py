0f1945d9f350d6a4b424f6e67d7146299678406d
Evan Strait <115466082+evanstrait@users.noreply.github.com>
2022-12-11 21:02:53 +0100

    Add files via upload

--- Module_1/Lab_31_Code-Simplicity-and-Efficiency/challenge-2.py
+++ Module_1/Lab_31_Code-Simplicity-and-Efficiency/challenge-2.py
@@ -7,32 +7,42 @@ The code is functional but has a lot of room for improvement. Use what you have 
 about simple and efficient code, refactor the code.
 """
 
-def RandomStringGenerator(l=12, a=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','0','1','2','3','4','5','6','7','8','9']):
-    p = 0
-    s = ''
-    while p<l:
-        import random
-        s += random.choice(a)
-        p += 1
-    return s
+# Declare all imports at the beginning of the file
+import random
+import string
+import sys
 
-def BatchStringGenerator(n, a=8, b=12):
+# Take out list of possible characters from function declaration
+# and use string module to generate it
+pwd_patt = string.ascii_lowercase + string.ascii_uppercase + string.digits
+
+def RandomStringGenerator(str_len=12):
+    # Transform while loop into list comprehension
+    s = [random.choice(pwd_patt) for i in range(str_len)]
+    # Return string
+    return ''.join(s)
+
+def BatchStringGenerator(nb_rand_str, min_str_len=8, max_str_len=12):
     r = []
-    for i in range(n):
-        c = None
-        if a < b:
-            import random
-            c = random.choice(range(a, b))
-        elif a == b:
-            c = a
+    for i in range(nb_rand_str):
+        str_len = None
+        if min_str_len <= max_str_len:
+            # debug: range() does not include stop point
+            # so no pwd of length of max_str_len will be generated
+            # we add 1 to solve this
+            # and so we can merge the conditions min < max and min = max
+            str_len = random.choice(range(min_str_len, max_str_len+1))
         else:
-            import sys
             sys.exit('Incorrect min and max string lengths. Try again.')
-        r.append(RandomStringGenerator(c))
+        # We keep for loop / append instead of list comprehension
+        # as it keeps the code more readable
+        r.append(RandomStringGenerator(str_len))
     return r
 
-a = input('Enter minimum string length: ')
-b = input('Enter maximum string length: ')
-n = input('How many random strings to generate? ')
+# No change to look & feel
+# Variables renamed to be senseful
+min_str_len = input('Enter minimum string length: ')
+max_str_len = input('Enter maximum string length: ')
+nb_rand_str = input('How many random strings to generate? ')
 
-print(BatchStringGenerator(int(n), int(a), int(b)))
+print(BatchStringGenerator(int(nb_rand_str), int(min_str_len), int(max_str_len)))
--- Module_1/Lab_31_Code-Simplicity-and-Efficiency/challenge-3.py
+++ Module_1/Lab_31_Code-Simplicity-and-Efficiency/challenge-3.py
@@ -12,19 +12,38 @@ The following function shows one way to solve the problem but the code is not id
 Refactor the code based on what you have learned about code simplicity and efficiency.
 """
 
-def my_function(X):
-    solutions = []
-    for x in range(5, X):
-        for y in range(4, X):
-            for z in range(3, X):
-                if (x*x==y*y+z*z):
-                    solutions.append([x, y, z])
-    m = 0
-    for solution in solutions:
-        if m < max(solution):
-            m = max(solution)
-    return m
+# Rename function
+def get_longest_side(max_side):
 
-X = input("What is the maximal length of the triangle side? Enter a number: ")
+    # Original algorithm was testing all possible triangles several times for each of them
+    # and the domain cardinality was a cube, resulting in long computations.
+    # Since we are looking for the max, we just need to stop when we find it.
+    # To ensure it is the max, we can start by the max value and then increment down.
+    # We can also order the values of the triangles x > y > z
+    # so we make sure we do not test the same triangle several times.
 
-print("The longest side possible is " + str(my_function(int(X))))
+    # We could even make it faster using Fermat's theorem
+    # and we first check if x can be expressed as the sum of two squares
+    # (if not, no need to run the y & z loops)
+    # but other labs await :)
+
+    # Fermat's theorem:
+    # A number N is expressible as a sum of 2 squares
+    # if and only if in the prime factorization of N,
+    # every prime of the form (4k+3) occurs an even number of times.
+
+        for x in range(max_side, 5, -1):
+            for y in range(x-1, 4, -1):
+                for z in range(y-1, 3, -1):
+                    if (x*x == y*y + z*z):
+                        print((x,y,z))
+                        # solution found, we can return it (which also breaks all loops)
+                        return x
+
+# Keep look & feel
+# Input variable renamed so it is senseful
+# Make it immediately an integer (will return an error if not a number)
+max_side = int(input("What is the maximal length of the triangle side? Enter number: "))
+
+# Use f-string as more readable and no need to convert return of function into string
+print(f"The longest side possible is {get_longest_side(max_side)}")
